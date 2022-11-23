07a9a4f73fb31815d8937446d0f43b2dca10ca04
sasso0101 <git@sasso.dev>
2022-11-23 16:36:46 +0100

    Text analysis

---
+++ Computer Programming 1/14 Dynamic structures/textAnalysis.cc
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+struct node {
+    string word;
+    int count;
+    node *lptr;
+    node *rptr;
+};
+
+node* addNode(string word, node* leaf);
+node* searchWord(string word, node& leaf);
+void printWords(node* root);
+
+node* addNode(string word, node* leaf = NULL) {
+    node* newLeaf = new node;
+    newLeaf->word = word;
+    newLeaf->count = 1;
+    newLeaf->lptr = NULL;
+    newLeaf->rptr = NULL;
+    if (leaf != NULL) {
+        if (word.compare(leaf->word) < 0) leaf->lptr = newLeaf;
+        else leaf->rptr = newLeaf;
+    }
+    return newLeaf;
+}
+
+node* searchWord(string word, node* root, node* &leaf) {
+    leaf = root;
+    int comp = word.compare(leaf->word);
+    while (
+        (comp < 0 && leaf->lptr != NULL) ||
+        (comp > 0 && leaf->rptr != NULL)
+    ) {
+        if (comp < 0) leaf = leaf->lptr;
+        else leaf = leaf->rptr;
+        comp = word.compare(leaf->word);
+    };
+    if (comp == 0) return leaf;
+    else return NULL;
+}
+
+/** If word alredy in tree increment it's counter by one
+    Else add word to tree
+*/
+node* insertOrSearch(string word, node* root = NULL) {
+    node *el = NULL;
+    if (root == NULL) el = addNode(word);
+    else {
+        node *leaf = NULL;
+        el = searchWord(word, root, leaf);
+        if (el != NULL) el->count += 1;
+        else el = addNode(word, leaf);
+    }
+    return el;
+}
+
+void printWords(node* root) {
+    if (root == NULL) return;
+    node *el = root;
+    cout << el->count << " " << el->word << endl;
+    printWords(el->lptr);
+    printWords(el->rptr);
+};
+
+int main() {
+    fstream file;
+    string word;
+    node *root = NULL;
+
+    file.open("file.txt");
+    while (file >> word)
+    {
+        if (root == NULL) root = insertOrSearch(word);
+        else insertOrSearch(word, root);
+    }
+    printWords(root);
+}
