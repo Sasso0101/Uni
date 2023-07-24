1177ada1c2b96e89506a64d04ae7483008ed8d96
sasso0101 <salvatore.andaloro01@gmail.com>
2023-07-24 09:31:27 +0200

    Monty hall

---
+++ Probability/Monty Hall paradox/paradox1.py
@@ -0,0 +1,25 @@
+from random import randrange
+
+totalwins = 0
+total = 0
+
+for i in range(1000):
+    win = randrange(3)
+    pick = randrange(3)
+
+    remove = randrange(3)
+    while remove == win or remove == pick:
+        remove = randrange(3)
+
+    # doors = [0,1,2]
+    # doors.remove(remove)
+    # if pick != remove: doors.remove(pick)
+    # pick = doors[0]
+
+    total += 1
+    if win == pick:
+        totalwins += 1
+
+print("Total wins: " + str(totalwins))
+print("Total games: " + str(total))
+print("Win percentage: " + str(totalwins/total))
