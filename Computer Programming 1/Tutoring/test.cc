d9582d19c2830c7096dc374a23f853f2ff585c1b
sasso0101 <salvatore.andaloro01@gmail.com>
2023-09-21 10:35:29 +0200

    Tutring

--- Computer Programming 1/Tutoring/test.cc
+++ Computer Programming 1/Tutoring/test.cc
@@ -1,11 +1,17 @@
+#include <bitset>
 #include <iostream>
 
 using namespace std;
 
-int test();
-
 int main() {
-    char i1 = 45;
-    cout << i1 << endl;
-    return 0;
+    char a = -12;
+    char b = 1;
+    bitset<8> x(a);
+    cout << x << '\n';
+    bitset<8> y(b);
+    cout << y << '\n';
+
+    char c = a & b;
+    bitset<8> z(c);
+    cout << z << '\n';
 }
