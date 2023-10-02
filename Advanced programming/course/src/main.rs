cac3db124f1514678b9c5c96834e6d0fdc0993fa
sasso0101 <salvatore.andaloro01@gmail.com>
2023-10-02 11:07:15 +0200

    Class material

--- Advanced programming/course/src/main.rs
+++ Advanced programming/course/src/main.rs
@@ -1,11 +1,28 @@
 #![allow(dead_code)]
 #![allow(unused)]
 
+use class7::{singlemap, twomaps, lazymap_collect, lazymap_nocollect, string_tolower, maps_options, mapsownership};
+
 mod class1;
 mod class2;
 mod class3;
 mod class4;
+mod class7;
 
-pub fn main() {
-    println!("Hello world!")
+pub fn main(){
+    foo("ciao".to_string());
+  }
+  fn foo(str:String) {
+    let x = str; 
+    let y = &x;
+    let z = x;
+    let w = &z;
+    let t = &z;
+    println!("{}", t);
+    println!("{}", w);
+    // HERE
 }
+
+// pub fn main() {
+//     // mapsownership();
+// }
