bff5874dd237111fc07f4a61428b355dc24e9b68
sasso0101 <salvatore.andaloro01@gmail.com>
2024-01-20 18:20:15 +0100

    Noted build instructions

--- Logic_networks/Calculator/build.tcl
+++ Logic_networks/Calculator/build.tcl
@@ -1,20 +1,22 @@
-set outputDir ./output         
-file mkdir $outputDir
-#
-# STEP#1: setup design sources and constraints
-#
-add_files calculator.vhd accumulator.vhd debouncer.vhd seven_segment_driver.vhd alu.vhd
+# Build guide
+# 1. Enable Vivado utility:
+#   . /opt/Vivado/Vivado/2023.2/settings64.sh
+# 2. Copy files to server:
+#   scp * user@host:~/calculator
+# 3. Generate bitstream (run command on server):
+#   vivado -mode batch -nolog -nojournal -source build.tcl
+# 4. Download bitstream:
+#   scp user@host:~/calculator/output/calculator.bit calculator.bit
+# 5. Flash on device:
+#   openFPGALoader -b basys3 -f calculator.bit
+
+add_files ./
 read_xdc "Basys3_Master.xdc"
 
-#
-# STEP#2: run synthesis, report utilization and timing estimates, write checkpoint design
-#
 synth_design -top calculator -part xc7a35tcpg236-1
 
 opt_design
 place_design
-# write_checkpoint -force $outputDir/post_place
 route_design
-# write_checkpoint -force $outputDir/post_route
 
-write_bitstream -force $outputDir/calculator.bit
+write_bitstream -force calculator.bit
