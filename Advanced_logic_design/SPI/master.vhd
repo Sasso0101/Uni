8fcbfc8a46ada4f2615b30e25cf7d7c7c139933c
sasso0101 <git@sasso.dev>
2024-05-16 00:32:53 +0200

    Needs refinements

--- Advanced_logic_design/SPI/master.vhd
+++ Advanced_logic_design/SPI/master.vhd
@@ -21,8 +21,10 @@ entity spi_master is
     load_data : in std_logic;
     -- Data: the data to be sent
     data : in std_logic_vector(7 downto 0);
-    -- Data currently in shift register
-    shift_data : out std_logic_vector( 7 downto 0 );
+    -- Data the be sent currently in shift register
+    data_tx : out std_logic_vector( 7 downto 0 );
+    -- Data received
+    data_rx : out std_logic_vector( 7 downto 0 );
     -- Command: start data transmission
     start : in std_logic;
     -- SPI interface signals (Master)
@@ -34,63 +36,51 @@ entity spi_master is
 end entity spi_master;
 
 architecture rtl of spi_master is
-  component shift_reg is
-    port
-    (
-      clk : in std_logic;
-      reset : in std_logic;
-      shift_enable : in std_logic;
-      shift_data: out std_logic_vector(7 downto 0);
-      load_data : in std_logic_vector(7 downto 0);
-      load_enable : in std_logic;
-      data_out : out std_logic
-    );
-  end component;
-  component counter is
-    generic (
-        counter_size : integer := 3
-    );
-    port (
-        clk : in std_logic;
-        reset : in std_logic;
-        count_enable : in std_logic;
-        tc : out std_logic
-    );
-  end component;
   type t_State is (Sending, Receiving, Idle);
   signal state : t_State := Idle;
-  signal shift_enable: std_logic := '0';
-  signal shift_load: std_logic_vector(7 downto 0) := (others => '0');
-  signal load_enable: std_logic := '0';
-  signal data_out: std_logic := '0';
+  signal shift_enable_tx: std_logic := '0';
+  signal shift_enable_rx: std_logic := '0';
   signal bit_count_en: std_logic := '0';
   signal bit_count_tc: std_logic := '0';
   signal clk_count_en: std_logic := '0';
   signal clk_count_tc: std_logic := '0';
-  signal data_in: std_logic_vector(7 downto 0);
+  signal data_tx_reg: std_logic := '0';
 begin
-  output_register : shift_reg
+  register_tx : entity work.shift_reg
   port map
   (
-    clk => clk,
+    clk => SCK,
     reset => reset,
-    shift_enable => shift_enable,
-    shift_data => shift_data,
-    load_data => shift_load,
-    load_enable => load_enable,
-    data_out => data_out
+    shift_enable => shift_enable_tx,
+    load_enable => load_data,
+    parallel_in => data,
+    data => data_tx,
+    data_in => '0',
+    data_out => data_tx_reg
   );
-  bit_counter: counter
+  register_rx : entity work.shift_reg
+  port map
+  (
+    clk => SCK,
+    reset => reset,
+    shift_enable => shift_enable_rx,
+    load_enable => '0',
+    parallel_in => (others => '0') ,
+    data => data_rx,
+    data_in => MISO,
+    data_out => open
+  );
+  bit_counter: entity work.counter
   generic map (
     counter_size => 3
   )
   port map (
-    clk => clk,
+    clk => SCK,
     reset => reset,
     count_enable => bit_count_en,
     tc => bit_count_tc
   );
-  clk_counter: counter
+  clk_counter: entity work.counter
   generic map (
     counter_size => 4
   )
@@ -101,40 +91,43 @@ begin
     tc => clk_count_tc
   );
 
-  process ( clk, reset, clk_count_tc )
+  process ( clk, reset )
   begin
+    clk_count_en <= '1';
+    
     if reset = '1' then
-      MOSI <= '0';
+      shift_enable_tx <= '0';
+      shift_enable_rx <= '0';
       SCK <= '0';
       SS <= '0';
+      MOSI <= 'Z';
     else
-      if rising_edge ( clk ) then
-        if state = Idle then
-          if load_data = '1' then
-            shift_load <= data;
-          elsif start = '1' then
-            bit_count_en <= '1';
-            shift_enable <= '1';
-            state <= Sending;
-          end if;
+      if rising_edge (clk) then
+        if clk_count_tc = '1' then
+          SCK <= not(SCK);
         end if;
-      elsif rising_edge( clk_count_tc ) then
         case state is
+          when Idle =>
+            if clk_count_tc = '1' and start = '1' then
+              bit_count_en <= '1';
+              shift_enable_tx <= '1';
+              state <= Sending;
+            end if;
           when Sending =>
+            MOSI <= data_tx_reg;
             if bit_count_tc = '1' then
-              shift_enable <= '0';
-              bit_count_en <= '0';
-              state <= Idle;
-            else
-              MOSI <= data_out;
+              shift_enable_tx <= '0';
+              -- bit_count_en <= '0';
+              MOSI <= 'Z';
+              shift_enable_rx <= '1';
+              state <= Receiving;
             end if;
           when Receiving =>
-            data_in <= data_in(data_in'high -1 downto data_in'low) & MISO;
             if bit_count_tc = '1' then
+              shift_enable_rx <= '0';
               bit_count_en <= '0';
               state <= Idle;
             end if;
-          when Idle =>
         end case;
       end if;
     end if;
--- Advanced_logic_design/SPI/master_tb.vhd
+++ Advanced_logic_design/SPI/master_tb.vhd
@@ -6,54 +6,27 @@ entity spi_master_tb is
 end entity;
 
 architecture rtl of spi_master_tb is
-  component spi_master is
-    generic
-    (
-      -- Number of bits to transfer
-      NUM  : natural   := 8;
-      CPOL : std_logic := '1';
-      CPHA : std_logic := '1'
-    );
-    port
-    (
-      -- System clock
-      clk : in std_logic;
-      -- System reset (active low)
-      reset : in std_logic;
-      -- Command: load the data to be sent
-      load_data : in std_logic;
-      -- Data: the data to be sent
-      data : in std_logic_vector(7 downto 0);
-      -- Data currently in shift register
-      shift_data : out std_logic_vector( 7 downto 0 );
-      -- Command: start data transmission
-      start : in std_logic;
-      -- SPI interface signals (Master)
-      MOSI : out std_logic;
-      MISO : in std_logic;
-      SCK  : out std_logic;
-      SS   : out std_logic
-    );
-  end component;
 
   signal clk : std_logic := '0';
   signal reset : std_logic := '0';
   signal load_data : std_logic := '0';
   signal data : std_logic_vector( 7 downto 0 ) := (others => '0');
-  signal shift_data : std_logic_vector( 7 downto 0 ) := (others => '0');
+  signal data_tx : std_logic_vector( 7 downto 0 ) := (others => '0');
+  signal data_rx : std_logic_vector( 7 downto 0 ) := (others => '0');
   signal start : std_logic := '0';
   signal MOSI : std_logic := '0';
   signal MISO : std_logic := '0';
   signal SCK : std_logic := '0';
   signal SS : std_logic := '0';
 begin
-    spi_master_0: spi_master
+    spi_master_0: entity work.spi_master
     port map (
         clk => clk,
         reset => reset,
         load_data => load_data,
         data => data,
-        shift_data => shift_data,
+        data_tx => data_tx,
+        data_rx => data_rx,
         start => start,
         MOSI => MOSI,
         MISO => MISO,
@@ -61,19 +34,18 @@ begin
         SS => SS
     );
 
-    clk <= not(clk) after 100 us;
+    clk <= not(clk) after 100 ns;
     process begin
         reset <= '1';
         wait for 10 us;
         reset <= '0';
-        data <= "01001010";
+        data <= "01001001";
         load_data <= '1';
         wait for 10 us;
         load_data <= '0';
         start <= '1';
-        wait for 1 us;
+        wait for 20 us;
         start <= '0';
-        reset <= '0';
         wait;
     end process;
 end architecture;
